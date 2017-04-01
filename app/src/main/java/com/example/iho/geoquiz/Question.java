de4f4ed6d1c1aec7048e5354dfb979a287261177
i1ana <bachwasbaroque@gmail.com>
2017-04-01 16:43:50 -0600

    solved prev button crash error

---
+++ app/src/main/java/com/example/iho/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package com.example.iho.geoquiz;
+
+/**
+ * Created by iho on 3/31/17.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue){
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
